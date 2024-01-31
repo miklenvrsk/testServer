d8544e391af68a40ee2aef33bab76772c4e45f24
Михаил Скворцов <miklenvrsk5@yandex.ru>
2024-01-31 20:44:54 +0300

    server

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+ 
+const app = express();
+ 
+app.set("view engine", "hbs");
+ 
+app.use("/contact", function(_, response){
+     
+    response.render("contact.hbs");
+});
+app.use("/", function(_, response){
+     
+    response.render("index.hbs");
+});
+app.listen(3000);
